d60dcd1ad2e662905924c3825d010cea1ac1c50c
Caio <caiooliveira0277@gmail.com>
2026-05-10 19:22:07 -0300

    grafos não direcionado

---
+++ meu_grafo_lista_adj_nao_dir.py
@@ -0,0 +1,129 @@
+from bibgrafo.grafo_lista_adj_nao_dir import GrafoListaAdjacenciaNaoDirecionado
+from bibgrafo.grafo_errors import *
+
+class MeuGrafo(GrafoListaAdjacenciaNaoDirecionado):
+
+    def vertices_nao_adjacentes(self):
+        '''
+        Provê um conjunto de vértices não adjacentes no grafo.
+        O conjunto terá o seguinte formato: {X-Z, X-W, ...}
+        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+        :return: Um objeto do tipo set que contém os pares de vértices não adjacentes
+        '''
+        naoAdj = set()
+        vertex = self.vertices
+        adjacencias = [{a.v1.rotulo, a.v2.rotulo} for a in self.arestas.values()]
+        TAMVERTEX = len(vertex)
+
+        
+        for i in range(TAMVERTEX - 1):
+            for j in range(i + 1, TAMVERTEX):
+                if {vertex[i].rotulo, vertex[j].rotulo} not in adjacencias:
+                    naoAdj.add(f"{vertex[i].rotulo}-{vertex[j].rotulo}")
+
+        return naoAdj
+        #OK
+
+    def ha_laco(self):
+        ''' 
+        Verifica se existe algum laço no grafo.
+        :return: Um valor booleano que indica se existe algum laço.
+        '''
+        for a in self.arestas:
+            # print(self.arestas[a])
+            if self.arestas[a].v1 == self.arestas[a].v2:
+                return True;
+        return False;
+        #OK
+
+    def grau(self, V=''):
+        '''
+        Provê o grau do vértice passado como parâmetro
+        :param V: O rótulo do vértice a ser analisado
+        :return: Um valor inteiro que indica o grau do vértice
+        :raises: VerticeInvalidoError se o vértice não existe no grafo
+        '''
+        if not(self.existe_rotulo_vertice(V)) : raise VerticeInvalidoError(f"O vertice {V} não está no grafo")
+
+        grau = 0
+        for a in self.arestas:
+            if self.arestas[a].v1.rotulo == self.arestas[a].v2.rotulo and V == self.arestas[a].v1.rotulo:
+                grau += 2
+            elif self.arestas[a].v1.rotulo == V or self.arestas[a].v2.rotulo == V:
+                grau += 1
+        return grau
+        #OK
+
+    def ha_paralelas(self):
+        '''
+        Verifica se há arestas paralelas no grafo
+        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+        '''
+        ini = 1
+        listaKeys = list(self.arestas.keys())
+        #print(f"{len(listaKeys)} | {listaKeys} \n\n")
+        #print(self.arestas)
+
+        for i in range(len(listaKeys) - 1):
+            for j in range(ini, len(listaKeys)):
+                if self.arestas[listaKeys[i]] == self.arestas[listaKeys[j]]:
+                    return True
+            ini += 1
+        return False
+        #OK
+
+    def arestas_sobre_vertice(self, V):
+        '''
+        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+        :param V: Um string com o rótulo do vértice a ser analisado
+        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        if not(self.existe_rotulo_vertice(V)) : raise VerticeInvalidoError(f"O vertice {V} não está no grafo")
+        adjacentes = set()
+        for i in self.arestas:
+            if self.arestas[i].v1.rotulo == V or self.arestas[i].v2.rotulo == V:
+                adjacentes.add(self.arestas[i].rotulo)
+        return adjacentes
+        #OK
+
+    def eh_completo(self):
+        '''
+        Verifica se o grafo é completo.
+        :return: Um valor booleano que indica se o grafo é completo
+        '''
+        
+        if self.ha_paralelas() or self.ha_laco() or (len(self.vertices_nao_adjacentes()) > 0):
+            return False
+        return True
+        #OK
+
+    def ha_ciclo(self):
+        # Retornar True caso haja um ciclo no grafo
+        #Recriando o grafo como uma lista de adjacencia tipo: {'1': [], '4': [], '3': [], '2': []}
+        grafoListaAdj = dict()
+        for v in self.vertices:
+            grafoListaAdj[v.rotulo] = list()
+
+        for a in self.arestas.values():
+            grafoListaAdj[a.v1.rotulo].append(a.v2.rotulo)
+            grafoListaAdj[a.v2.rotulo].append(a.v1.rotulo)
+
+        #DFS para encontrar ciclo
+        visitados = set()
+        vertice = list(self.arestas.keys())[0]
+
+        def dfsParaCiclo(v, pai):
+            visitados.add(v)
+    
+            for vizin in grafoListaAdj[v]:
+                if vizin not in visitados:
+                    if dfsParaCiclo(vizin, v): # v pai
+                        return True
+                    
+                elif vizin != pai:
+                    return True
+            
+            return False 
+        
+        return dfsParaCiclo(vertice, None)
---
+++ teste.py
@@ -0,0 +1,64 @@
+from bibgrafo.grafo_lista_adj_nao_dir import GrafoListaAdjacenciaNaoDirecionado
+
+graph = GrafoListaAdjacenciaNaoDirecionado()
+graph.adiciona_vertice("1")
+graph.adiciona_vertice("4")
+graph.adiciona_vertice("3")
+graph.adiciona_vertice("2")
+
+graph.adiciona_aresta("a1", "1", "2")
+graph.adiciona_aresta("a2", "2", "3")
+graph.adiciona_aresta("a3", "3", "1")
+
+grafoListaAdj = dict()
+for v in graph.vertices:
+    grafoListaAdj[v.rotulo] = list()
+
+for a in graph.arestas.values():
+    grafoListaAdj[a.v1.rotulo].append(a.v2.rotulo)
+    grafoListaAdj[a.v2.rotulo].append(a.v1.rotulo)
+
+print(graph)
+print("--" * 5, "\n", grafoListaAdj)
+
+'''
+grafo = {'1': ['4', '3'], '4': [], '3': [], '2': []}
+visitados = set()
+vertice = list(grafo.keys())[0]
+ciclo = True
+
+def dfsParaCiclo(v, pai):
+    visitados.add(v)
+    
+    for vizin in grafo[v]:
+        if vizin not in visitados:
+            if dfsParaCiclo(vizin, v):
+                return True
+
+        elif vizin != pai:
+            return True
+    
+    return False 
+'''
+#conj = {graph.vertices[0].rotulo, graph.vertices[1].rotulo}
+#par_atual = { frozenset([2, 3]), frozenset([4, 5]) }
+'''
+naoAdj = set()
+if len(naoAdj) == 0:
+    print("a")
+else:
+    print("B")
+
+
+naoAdj = set()
+vertex = graph.vertices
+adjacencias = [{i.v1.rotulo, i.v2.rotulo} for i in graph.arestas.values()]
+for i in range(len(vertex) - 1):
+    for j in range(c, len(vertex)):
+        if {vertex[i].rotulo, vertex[j].rotulo} not in adjacencias:
+            naoAdj.add(f"{vertex[i].rotulo}-{vertex[j].rotulo}")
+    c += 1
+    print(naoAdj)
+
+'''
+#python -m unittest grafo_lista_adj_test_nao_dir.TestGrafo.test_ha_paralelas
